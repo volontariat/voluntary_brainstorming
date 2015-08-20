e40985e1a3236c8261a7a75a0bf0fc65fa2ce684
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-20 21:26:39 +0200

    Sets cascading.

--- app/models/brainstorming_idea.rb
+++ app/models/brainstorming_idea.rb
@@ -2,8 +2,8 @@ class BrainstormingIdea < ActiveRecord::Base
   belongs_to :brainstorming
   belongs_to :user
   
-  has_many :arguments, as: :argumentable
-  has_many :votes, class_name: 'BrainstormingIdeaVote'
+  has_many :arguments, as: :argumentable, dependent: :destroy
+  has_many :votes, class_name: 'BrainstormingIdeaVote', dependent: :destroy, foreign_key: 'idea_id'
   
   validates :brainstorming_id, presence: true
   validates :user_id, presence: true
